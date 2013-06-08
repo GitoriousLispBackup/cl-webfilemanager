7054e4ff1b5780eb5fbdd1b99de5b42a0e8530a2
Philippe Brochard <hocwp@free.fr>
2013-06-08 23:54:57 +0200

    Add a light xbmc version

--- xbmc.lisp
+++ xbmc.lisp
@@ -115,3 +115,114 @@
 (define-easy-handler (xbmc-context :uri "/xbmc-context") ()
   (fdo-shell (curl-cmd "Input.ContextMenu"))
   (xbmc-remote-page))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Light version
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun main-light-js-code ()
+  (ps
+    (defun url-go-to (url)
+      (setf (@ (chain window location href)) url))
+
+    (setf (@ document onkeydown)
+          (lambda (e)
+            (let ((keycode (getprop e 'key-code)))
+              ;;(setf (@ (chain document (get-element-by-id "plop")) inner-h-t-m-l) keycode)
+              (case keycode
+                (39 (url-go-to "/xbmcl-right"))
+                (37 (url-go-to "/xbmcl-left"))
+                (38 (url-go-to "/xbmcl-up"))
+                (40 (url-go-to "/xbmcl-down"))
+                (33 (url-go-to "/xbmcl-page_up"))
+                (34 (url-go-to "/xbmcl-page_down"))
+                (8 (url-go-to "/xbmcl-back"))
+                (27 (url-go-to "/xbmcl-back"))
+                (32 (url-go-to "/xbmcl-context"))
+                (17 (url-go-to "/xbmcl-context"))
+                (72 (url-go-to "/xbmcl-home"))
+                (36 (url-go-to "/xbmcl-home"))
+                (13 (url-go-to "/xbmcl-select"))))))))
+
+
+;;    (defun handle-key-down (e)
+;;      ;;(alert (getprop e 'key-code)))))
+;;      (setf (@ (chain document (get-element-by-id "plop")) inner-h-t-m-l) (getprop e 'key-code))
+;;      true)))
+
+;; Home   Up   Back
+;; Left Select Right
+;; PgUp  Down  PgDn
+;; Context
+(defun xbmc-light-remote-page ()
+  (with-html-output-to-string (*standard-output* nil :prologue t)
+    (:html
+     (:head (:title "XBMC Remote")
+            (:script :type "text/javascript"
+                     (str (main-light-js-code))))
+     (:body
+      (:a :href "xbmcl-home" "Home") " "
+      (:a :href "xbmcl-up" "Up") " "
+      (:a :href "xbmcl-back" "Back")
+      (:br)
+      (:a :href "xbmcl-left" "Left") " "
+      (:a :href "xbmcl-select" "Select") " "
+      (:a :href "xbmcl-right" "Right") (:br)
+      (:a :href "xbmcl-page_up" "PgUp") " "
+      (:a :href "xbmcl-down" "Down") " "
+      (:a :href "xbmcl-page_down" "PgDown") (:br)
+      (:a :href "xbmcl-context" "Context")
+      ;;(:span :id "plop" "plop")
+      ;;(:input :type "text" :onkeydown (ps (handle-key-down event)))
+      ))))
+
+;; {"jsonrpc": "2.0", "method": "Playlist.Add","params": {"playlistid": 1, "item": {"file" : "smb://homestorage/XYZ.JPG"}}, "id": 1}
+
+
+(define-easy-handler (xbmc-light :uri "/xbmcl") ()
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-up :uri "/xbmcl-up") ()
+  (fdo-shell (curl-cmd "Input.Up"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-down :uri "/xbmcl-down") ()
+  (fdo-shell (curl-cmd "Input.Down"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-left :uri "/xbmcl-left") ()
+  (fdo-shell (curl-cmd "Input.Left"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-right :uri "/xbmcl-right") ()
+  (fdo-shell (curl-cmd "Input.Right"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-back :uri "/xbmcl-back") ()
+  (fdo-shell (curl-cmd "Input.Back"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-home :uri "/xbmcl-home") ()
+  (fdo-shell (curl-cmd "Input.Home"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-select :uri "/xbmcl-select") ()
+  (fdo-shell (curl-cmd "Input.Select"))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-page_down :uri "/xbmcl-page_down") ()
+  (dotimes (i 10)
+    (fdo-shell (curl-cmd "Input.Down")))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-page_up :uri "/xbmcl-page_up") ()
+  (dotimes (i 10)
+    (fdo-shell (curl-cmd "Input.Up")))
+  (xbmc-light-remote-page))
+
+(define-easy-handler (xbmc-light-context :uri "/xbmcl-context") ()
+  (fdo-shell (curl-cmd "Input.ContextMenu"))
+  (xbmc-light-remote-page))
