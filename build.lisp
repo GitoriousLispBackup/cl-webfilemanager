41e26cac6a7d332cf635a7518601984e00448ab5
Philippe Brochard <hocwp@free.fr>
2013-06-08 23:14:39 +0200

    Load parenscript from load.lisp or build.lisp

--- build.lisp
+++ build.lisp
@@ -1,6 +1,7 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload "hunchentoot")
 (ql:quickload "cl-who")
+(ql:quickload "parenscript")
 
 (load "cl-webfilemanager.asd")
 (require :cl-webfilemanager)
--- load.lisp
+++ load.lisp
@@ -1,6 +1,7 @@
 ;;(load "~/Lisp/quicklisp-ccl/setup.lisp")
 (ql:quickload "hunchentoot")
 (ql:quickload "cl-who")
+(ql:quickload "parenscript")
 
 (load "cl-webfilemanager.asd")
 (require :cl-webfilemanager)
